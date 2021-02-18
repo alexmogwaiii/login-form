694941bd9976ac2921ee2d5775d968cbca01dd83
Alex <alexmogwaiii@gmail.com>
2021-02-18 20:36:53 +0200

    implemented responsive design to page

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 import React, { useState } from 'react';
 import { makeStyles, withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
@@ -9,6 +8,7 @@ import InputAdornment from '@material-ui/core/InputAdornment';
 import IconButton from '@material-ui/core/IconButton';
 import Visibility from '@material-ui/icons/Visibility';
 import VisibilityOff from '@material-ui/icons/VisibilityOff';
+import FormHelperText from '@material-ui/core/FormHelperText';
 
 import './Login.scss';
 
@@ -17,6 +17,7 @@ const ColorButton = withStyles((theme) => ({
     width: '212px',
     marginTop: '64px',
     height: '48px',
+    fontWeight: '500',
     color: '#FEFEFE',
     background: 'linear-gradient(90deg, #FF9146 0%, #FF351B 100%)',
     '&:hover': {
@@ -26,8 +27,12 @@ const ColorButton = withStyles((theme) => ({
       height: '40px',
       width: '163px',
       marginTop: '50px',
-    }
+    },
   },
+  disabled: {
+    background: '#B2B7BB',
+    color: '#FEFEFE !important',
+  }
 }))(Button);
 
 const useStyles = makeStyles((theme) => ({
@@ -37,98 +42,145 @@ const useStyles = makeStyles((theme) => ({
     borderRadius: '10px',
     [theme.breakpoints.down('xs')]: {
       height: '40px',
-    }
+    },
   },
   outlined: {
     transform: 'translate(14px, 17px)',
     [theme.breakpoints.down('xs')]: {
       transform: 'translate(14px, 14px)'
     }
-  }
+  },
+  error: {
+    position: 'absolute',
+    top: '50px',
+    fontSize: '14px',
+  },
 }));
 
 export const Login = () => {
   const classes = useStyles();
-  const [queryEmail, setEmail] = useState('');
-  const [queryPassword, setPassword] = useState('');
+  const [email, setEmail] = useState({
+    query: '',
+    error: false,
+  });
+  const [password, setPassword] = useState({
+    query: '',
+    error: false,
+  });
   const [isPasswordShown, setPasswordShown] = useState(false);
 
   const togglePasswordShown = () => {
     setPasswordShown(!isPasswordShown);
   };
 
-  const handlePassword = (event) => {
-    setPassword(event.target.value);
-  }
-
   const handleChange = (event) => {
     const { id, value } = event.target;
-    
+
     switch (id) {
       case 'email':
-        setEmail(value);
+        setEmail({error: false, query: value});
         break;
       case 'password':
-        setPassword(value);
+        setPassword({error: false, query: value});
         break;
       default:
         break;
     }
   };
 
-  console.log(queryEmail, queryPassword);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!password.query || !email.query) {
+      setEmail({ ...email, error: !email.query });
+      setPassword({ ...password, error: !password.query });
+    }
+  }
 
   return (
     <div className="login">
       <h1 className="login__title">Login</h1>
 
-      <form className="login__form">
+      <form
+        className="login__form"
+        onSubmit={handleSubmit}
+      >
         <div className="login__inputs">
           <FormControl variant="outlined">
             <InputLabel
               htmlFor="email"
               className={classes.outlined}
+              error={email.error}
             >
               Email address
             </InputLabel>
             <OutlinedInput
               id="email"
-              value={queryEmail}
+              value={email.query}
               onChange={handleChange}
               labelWidth={100}
               className={classes.root}
+              error={email.error}
             />
+            {email.error
+              && <FormHelperText
+                  error
+                  className={classes.error}
+                 >
+                  Email required
+                 </FormHelperText>
+            }
           </FormControl>
           <FormControl variant="outlined">
             <InputLabel
               htmlFor="outlined-adornment-password"
               className={classes.outlined}
+              error={password.error}
             >
               Password
             </InputLabel>
             <OutlinedInput
-            id="password"
-            type={isPasswordShown ? 'text' : 'password'}
-            value={queryPassword}
-            onChange={handlePassword}
-            className={classes.root}
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={togglePasswordShown}
-                  onMouseDown={togglePasswordShown}
-                  edge="end"
-                >
-                  {isPasswordShown ? <Visibility /> : <VisibilityOff />}
-                </IconButton>
-              </InputAdornment>
+              id="password"
+              type={isPasswordShown ? 'text' : 'password'}
+              value={password.query}
+              onChange={handleChange}
+              className={classes.root}
+              error={password.error}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={togglePasswordShown}
+                    onMouseDown={togglePasswordShown}
+                    edge="end"
+                  >
+                    {isPasswordShown
+                      ? <Visibility 
+                          color={password.error ? 'error' : 'action'}
+                        />
+                      : <VisibilityOff
+                          color={password.error ? 'error' : 'action'}
+                        />}
+                  </IconButton>
+                </InputAdornment>
+              }
+              labelWidth={70}
+            />
+            {password.error
+              && <FormHelperText
+                  error
+                  className={classes.error}
+                 >
+                  Password required
+                 </FormHelperText>
             }
-            labelWidth={70}
-          />
           </FormControl>
         </div>
-        <ColorButton variant="contained">
+        <ColorButton
+          type="submit"
+          variant="contained"
+          disabled={email.error || password.error}
+        >
             Log in
         </ColorButton>
       </form>
